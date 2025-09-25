1a2a7b6d8ea26b3576f754119543ab91abb0d77f
Qedge <Qedge@DESKTOP-1RLTEAV.Dlink>
2025-09-25 10:49:52 +0530

    New Class Added

---
+++ DataDriven_Framework/src/test/java/commonFunctions/Demo.java
@@ -0,0 +1,26 @@
+package commonFunctions;
+
+public class Demo {
+public void add()
+{
+	int a=4566;
+	int b=456;
+	int c=a+b;
+	System.out.println(c);
+}
+
+public void div()
+{
+	int a=67676;
+	int b=5;
+	int c=a/b;
+	System.out.println(c);
+}
+public void sub()
+{
+	int a=543654;
+	int b=89;
+	int c=a*b;
+	System.out.println(c);
+}
+}
